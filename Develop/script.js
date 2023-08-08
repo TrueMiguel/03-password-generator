5512a95e049bf62c23da8e072f60077f2c642f7a
Miguel M <miguel.maldonado5802@gmail.com>
2023-08-08 11:16:08 -0400

    Added array for password elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,45 @@
 // Assignment code here
+// creating the array
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+// checking if the array works
+console.log(lettersLower)
+console.log(lettersUpper)
+console.log(num)
+console.log(specialChar)
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   // this is broken when trying to run writePassword
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-  // creating the prompts for the password requirements
-  var passwordLenght = prompt("Please choose lenght fo the password. 8 to 128 characters only")
-  alert("Please decide on the following character types that will make up the password")
-  var typeLower = confirm("Do you want lower case characters?")
-  var typeUpper = confirm("Do you want upper case characters?")
-  var typeNum = confirm("Do you want numeric characters?")
-  var typeSpecial = confirm ("Do you want special characters?")
-  // checking that the prompts work
-  console.log(passwordLenght)
-  console.log(typeLower)
-  console.log(typeUpper)
-  console.log(typeNum)
-  console.log(typeSpecial)
-}
+//   // creating the prompts for the password requirements
+//   var passwordLenght = prompt("Please choose lenght fo the password. 8 to 128 characters only")
+//   alert("Please decide on the following character types that will make up the password")
+//   var typeLower = confirm("Do you want lower case characters?")
+//   var typeUpper = confirm("Do you want upper case characters?")
+//   var typeNum = confirm("Do you want numeric characters?")
+//   var typeSpecial = confirm ("Do you want special characters?")
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
-// need to make button work and when pressed it will generate a password
+// // need to make button work and when pressed it will generate a password
 
-// multiple window.prompt() for  the password criteria
-  // this is password lenght 8-128 characters
-  // if to include character types
-  // ask wether or not to include lowercase, uppercase, numeric, and or special characters.
+// // multiple window.prompt() for  the password criteria
+//   // this is password lenght 8-128 characters
+//   // if to include character types
+//   // ask wether or not to include lowercase, uppercase, numeric, and or special characters.
 
-writePassword()  
+// writePassword()  
+
